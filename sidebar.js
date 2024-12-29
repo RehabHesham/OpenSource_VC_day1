335b193808f96111582a2d1c83b420b502b8acdf
RehabHesham <rehabhesham98@yahoo.com>
2024-12-29 12:10:16 +0200

    extend sidebar logic

--- sidebar.js
+++ sidebar.js
@@ -1,1 +1,13 @@
 console.log ("Create Sidebar Logic");
+
+var hideSataus = true;
+
+function toggleSidebar() {
+    if (hideSataus) {
+        console.log("Show Sidebar");
+        hideSataus = false;
+    }else{
+        console.log("Hide Sidebar");
+        hideSataus = true;
+    }
+}
